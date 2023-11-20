8fe884168d1afc7a0c4f7c8f4ec796aa2083b493
Luis Miguel Sanchez Velazquez <lmsv930213@gmail.com>
2023-11-20 21:02:51 +0200

    Rename files (remove middleware suffix)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from "./middlewares/build-model.middleware";
-export * from "./middlewares/build-where-query.middleware";
+export * from "./middlewares/build-model";
+export * from "./middlewares/build-where-query";
 export * from "./middlewares/order-model";
