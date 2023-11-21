4acf58d4cb896d36927784bb228d8ea33a5415a1
orozco92 <goldstein.david92@gmail.com>
2023-11-21 08:10:20 -0500

    * Update typings

--- src/core/types/where-type.ts
+++ src/core/types/where-type.ts
@@ -2,5 +2,14 @@ export type WhereType = {
   /**
    * Any set of properties with values to query the model
    */
-  [key: string]: any;
-}
+  [key: string]: WherePrimitives | WhereArrays | WhereType;
+};
+
+/**
+ * Primitive values definitions
+ */
+export type WherePrimitives = string | number | Date | boolean | null;
+/**
+ * Array types definition
+ */
+export type WhereArrays = string[] | number[] | Date[] | boolean[];
