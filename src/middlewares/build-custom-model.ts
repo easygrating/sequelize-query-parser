e9ab920b1d3b89bb1d04872c14aaa88663d66481
orozco92 <goldstein.david92@gmail.com>
2023-12-05 15:37:00 -0500

    * Fix PR comments

--- src/middlewares/build-custom-model.ts
+++ src/middlewares/build-custom-model.ts
@@ -2,25 +2,29 @@ import { Response, NextFunction } from "express";
 import { SequelizeQueryParserRequestInterface } from "../core/interfaces/sequelize-query-parser-request.interface";
 import * as nmodulesLoader from "@easygrating/nmodules-loader";
 import { CustomModelMiddlewareInterface } from "../core/interfaces/custom-model-middleware.interface";
+import path from "path";
 
 /**
  * Middleware to call a custom middleware for a given route.
  * If no middleware is found it will call next function.
  * Middlewares must be an object of type CustomModelMiddlewareInterface.
- * It will call the first middleware that has route param equals to req.params.model
- * or modelName is equal to the loaded model name in the request object.
+ * It will call the first middleware that has route property equals to req.params.model
+ * or modelName property is equal to the loaded model name in the request object.
  *
  * @param {string} path directory location for custom models
- * @returns custom express middleware for a given modelName or a route model parameter
+ * @returns custom express middleware for a given modelName parameter or a route parameter
  */
-export function buildCustomModel(path?: string) {
+export function buildCustomModel(pathDir?: string) {
   return async (
     req: SequelizeQueryParserRequestInterface,
     res: Response,
     next: NextFunction
   ) => {
+    if (!pathDir) {
+      pathDir = path.resolve("./src/customMiddlewares");
+    }
     const middlewares: CustomModelMiddlewareInterface[] =
-      nmodulesLoader.loadModules(path);
+      nmodulesLoader.loadModules(pathDir);
     const custom = middlewares.find(
       (item) =>
         item.route === req.params.model ||
